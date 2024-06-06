dc1ac793967613f14fb96f86650f1fdcc341c0f3
darshil929 <darshilt929@gmail.com>
2024-06-07 02:59:23 +0530

    applied butterworth lowpass filter

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+from DataTransformation import LowPassFilter, PrincipalComponentAnalysis
 
 df = pd.read_pickle("../../data/interim/02_outliers_removed_chauvenet.pkl")
 
@@ -38,4 +39,26 @@ duration_df = df.groupby(["category"])["duration"].mean()
 duration_df.iloc[0] / 5
 duration_df.iloc[1] / 10
 
+# Applying Butterworth lowpass filter
+df_lowpass = df.copy()
+LowPass = LowPassFilter()
+
+fs = 1000 / 200
+cutoff = 1.3
+
+df_lowpass = LowPass.low_pass_filter(df_lowpass, "acc_y", fs, cutoff, order=5)
+
+subset = df_lowpass[df_lowpass["set"] == 45]
+print(subset["label"][0])
+
+fig, ax = plt.subplots(nrows=2, sharex=True, figsize = [20, 10])
+ax[0].plot(subset["acc_y"].reset_index(drop = True), label = "raw data")
+ax[1].plot(subset["acc_y_lowpass"].reset_index(drop = True), label = "butterworth filter")
+ax[0].legend(loc = "upper center", bbox_to_anchor = (0.5, 1.5), fancybox = True, shadow = True)
+ax[1].legend(loc = "upper center", bbox_to_anchor = (0.5, 1.5), fancybox = True, shadow = True)
+
+for col in predictor_columns:
+    df_lowpass = LowPass.low_pass_filter(df_lowpass, col, fs, cutoff, order=5)
+    df_lowpass[col] = df_lowpass[col + "_lowpass"]
+    del df_lowpass[col + "_lowpass"]
 
