c29d89d0e4744735c0a78a81fe704ca16134753d
darshil929 <darshilt929@gmail.com>
2024-06-09 01:45:28 +0530

    added: function to count reps

--- src/features/count_repetitions.py
+++ src/features/count_repetitions.py
@@ -54,3 +54,29 @@ bench_set["acc_r"].plot()
 
 column = "acc_r"
 LowPass.low_pass_filter(bench_set, col=column, sampling_frequency=fs, cutoff_frequency=0.4, order=10)[column + "_lowpass"].plot()
+
+# Creating function to count number of repetitions
+def count_reps(dataset, cutoff=0.4, order=10, column="acc_r"):
+    data = LowPass.low_pass_filter(dataset, col=column, sampling_frequency=fs, cutoff_frequency=cutoff, order=order)
+
+    indexes = argrelextrema(data[column + "_lowpass"].values, np.greater)
+
+    peaks = data.iloc[indexes]
+    
+    fig, ax = plt.subplots()
+    plt.plot(dataset[f"{column}_lowpass"])
+    plt.plot(peaks[f"{column}_lowpass"], "o", color="red")
+    ax.set_ylabel(f"{column}_lowpass")
+    exercise = dataset["label"].iloc[0].title()
+    category = dataset["category"].iloc[0].title()
+    plt.title(f"{category} {exercise}: {len(peaks)} Reps")
+    plt.show()
+    
+    return len(peaks)
+
+# Experimenting
+count_reps(bench_set, cutoff=0.4)
+count_reps(squat_set, cutoff=0.35)
+count_reps(row_set, cutoff=0.65, column="gyr_x")
+count_reps(ohp_set, cutoff=0.35)
+count_reps(dead_set, cutoff=0.4)
