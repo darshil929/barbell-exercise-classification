dcf7b5828e9cbe9d9f80da786af37e94bf6f08fe
darshil929 <darshilt929@gmail.com>
2024-06-09 01:14:31 +0530

    smoothened original data for counting repetitions

---
+++ src/features/count_repetitions.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from DataTransformation import LowPassFilter
+from scipy.signal import argrelextrema
+from sklearn.metrics import mean_absolute_error
+
+pd.options.mode.chained_assignment = None
+
+# Plot settings
+plt.style.use("fivethirtyeight")
+plt.rcParams["figure.figsize"] = (20, 5)
+plt.rcParams["figure.dpi"] = 100
+plt.rcParams["lines.linewidth"] = 2
+
+df = pd.read_pickle("../../data/interim/01_data_processed.pkl")
+df = df[df["label"] != "rest"]
+
+acc_r = df["acc_x"] ** 2 + df["acc_y"] ** 2 + df["acc_z"] ** 2
+gyr_r = df["gyr_x"] ** 2 + df["gyr_y"] ** 2 + df["gyr_z"] ** 2
+df["acc_r"] = np.sqrt(acc_r)
+df["gyr_r"] = np.sqrt(gyr_r)
+
+# splitting the data by exercises
+bench_df = df[df["label"] == "bench"]
+squat_df = df[df["label"] == "squat"]
+row_df = df[df["label"] == "row"]
+ohp_df = df[df["label"] == "ohp"]
+dead_df = df[df["label"] == "dead"]
+
+#visualizing the data and identifying the patterns
+plot_df = bench_df
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_x"].plot()
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_y"].plot()
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_z"].plot()
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["acc_r"].plot()
+
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_x"].plot()
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_y"].plot()
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_z"].plot()
+plot_df[plot_df["set"] == plot_df["set"].unique()[0]]["gyr_r"].plot()
+
+# Applying low pass filter to remove noise and smoothen the data
+fs = 1000 / 200
+LowPass = LowPassFilter()
+
+bench_set = bench_df[bench_df["set"] == bench_df["set"].unique()[0]]
+squat_set = squat_df[squat_df["set"] == squat_df["set"].unique()[0]]
+row_set = row_df[row_df["set"] == row_df["set"].unique()[0]]
+ohp_set = ohp_df[ohp_df["set"] == ohp_df["set"].unique()[0]]
+dead_set = dead_df[dead_df["set"] == dead_df["set"].unique()[0]]
+
+bench_set["acc_r"].plot()
+
+column = "acc_r"
+LowPass.low_pass_filter(bench_set, col=column, sampling_frequency=fs, cutoff_frequency=0.4, order=10)[column + "_lowpass"].plot()
